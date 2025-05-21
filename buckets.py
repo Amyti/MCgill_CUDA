791488bf35f296359e4bd5a95ff7a5126c9467ef
Amir KABBOURI <amir.kabbouri@etu.u-pec.fr>
2025-05-21 17:19:58 +0200

    Actualiser buckets.py

--- buckets.py
+++ buckets.py
@@ -74,22 +74,30 @@ testloader  = torch.utils.data.DataLoader(testset,  batch_size=64, shuffle=False
 class sANN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1,  64, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(1,  64, 3, padding=1)
         self.bn1   = nn.BatchNorm2d(64)
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.bn2   = nn.BatchNorm2d(128)
-        self.conv3 = nn.Conv2d(128,256, kernel_size=3, padding=1)
-        self.bn3   = nn.BatchNorm2d(256)
         self.pool  = nn.MaxPool2d(2)
+
+        self.conv2 = nn.Conv2d(64, 128, 3, padding=1)
+        self.bn2   = nn.BatchNorm2d(128)
+
+        self.conv3 = nn.Conv2d(128,256, 3, padding=1)
+        self.bn3   = nn.BatchNorm2d(256)
+
+        # on pool après CHAQUE conv
+        # 28→14→7→3
         self.fc1   = nn.Linear(256*3*3, 512)
         self.drop  = nn.Dropout(0.4)
         self.fc2   = nn.Linear(512, 10)
 
     def forward(self, x):
         x = torch.relu(self.bn1(self.conv1(x)))
-        x = self.pool(torch.relu(self.bn2(self.conv2(x))))
-        x = self.pool(torch.relu(self.bn3(self.conv3(x))))
-        x = x.view(x.size(0), -1)
+        x = self.pool(x)                              # 28→14
+        x = torch.relu(self.bn2(self.conv2(x)))
+        x = self.pool(x)                              # 14→7
+        x = torch.relu(self.bn3(self.conv3(x)))
+        x = self.pool(x)                              # 7→3
+        x = x.view(x.size(0), -1)                     # now 256*3*3
         x = torch.relu(self.fc1(x))
         x = self.drop(x)
         return self.fc2(x)
